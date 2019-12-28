29a45f17cebd6341253559af99863d6ff81004b0
singhsanket143 <singhsanket143@gmail.com>
2019-12-28 18:32:33 +0530

    Feat: Added new functionality to create notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191228103743) do
+ActiveRecord::Schema.define(version: 20191228124148) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id"
+    t.boolean "is_completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
